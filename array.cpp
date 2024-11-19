3d1eb3d7fcdebdb0d89d6b805993e02f051c0141
hariom1231 <coe@csmu.ac.in>
2024-11-19 14:54:48 +0530

    adding a cpp file

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a,c;
+	printf("enter the limit of array");
+	scanf("%d",&a);
+	int b[a];
+	for(c=0;c<a;c++){
+		printf("enter the number %d:",c+1);
+		scanf("%d",&b[c]);
+	//hello
+	
+}
+for(c=0;c<a;c++){
+
+printf("your number %d=%d \n",c+1,b[c]);	
+}
+}  
